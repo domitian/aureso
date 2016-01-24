81f4059e7074795d1ae9e01f7a5a5b7dadf67b31
bobba surendranath chowdary <surendranath@elitmus.com>
2016-01-25 00:12:59 +0530

    Added PricingPolicy concern to the models, which calculates the total price for model type based on pricing policy,using dummy methods for now and fix

---
+++ app/models/concerns/pricing_policy.rb
@@ -0,0 +1,30 @@
+module PricingPolicy
+    extend ActiveSupport::Concern
+
+    def calculate_total_price
+        PriceCalculator.new(self.model.organisation.pricing_policy,self.base_price).get_price
+    end
+
+    class PriceCalculator
+
+        def initialize policy
+            @policy = policy
+            @base_price = base_price
+        end
+
+        def get_price
+            if @policy == "flexible"
+                margin = rand*100.to_i
+                price = @base_price *margin
+            elsif @policy == "fixed"
+                margin = 1
+                price = @base_price + margin
+            elsif @policy == "prestige"
+                margin = 100
+                price = @base_price + margin
+            end
+            price
+        end
+    end
+
+end
